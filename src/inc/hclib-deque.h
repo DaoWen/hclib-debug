439271c55c57c071c050ad41b413ffacf9bfe149
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2016-11-03 21:58:09 -0500

    Increase the default deque size

--- src/inc/hclib-deque.h
+++ src/inc/hclib-deque.h
@@ -46,7 +46,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /* DEQUE API                                        */
 /****************************************************/
 
-#define INIT_DEQUE_CAPACITY 8096
+#define INIT_DEQUE_CAPACITY (1<<14) // 16k
 
 typedef struct deque_t {
     _Atomic int head;
