4054e1ac2f2031d82e54c334a43be3c3ec5f1f59
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-04-01 14:20:57 -0500

    fix debug script

--- test/cpp/test_all.sh
+++ test/cpp/test_all.sh
@@ -9,7 +9,7 @@ for f in `cat targets.txt`; do
     [[ -x $f && ! -d $f ]]
     echo "========== Running $f =========="
     if ! ./$f; then
-        bash debug.bash ./$f
+        bash ../../debug.bash ./$f
     fi
     echo
 done
